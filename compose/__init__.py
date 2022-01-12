ab6db2a0776edfe147853979ad6c1177962480a3
duskvirkus <duskvirkus@protonmail.com>
2022-01-12 13:58:25 -0700

    cleaning up api

--- compose/__init__.py
+++ compose/__init__.py
@@ -1,9 +1,3 @@
-from .in_out.load import load
-from .in_out.exporter import Exporter
-from .composition import Composition
-from .random import random_curve, random_curve_composition
-from .renderer.default import DefaultRenderer
-
 __version__ = "0.0.1"
 
 try:
@@ -18,7 +12,21 @@ else:
 
 if mode == 'normal':
     import os
+    from .in_out.load import load
+    from .in_out.exporter import Exporter
+    from .composition import Composition
+    from .random import random_curve, random_curve_composition
+    from .renderer.default import DefaultRenderer
+    from .image import Image
 
     global exporter
+    global renderer
 
     exporter = Exporter(os.path.join(os.getcwd(), 'exports'))
+    renderer = DefaultRenderer()
+
+    def render(comp: Composition) -> Image:
+        return renderer.render(comp)
+
+    def save(img: Image) -> None:
+        exporter.save(img)
